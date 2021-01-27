6bf3bfa08dd55bd3a5a8685f9896b71aa35eedd8
Rika Umra <rikaumra@gmail.com>
2021-01-27 15:32:12 +0200

    first commit

---
+++ src/main/java/com/zen/OnlineTutorApp/OnlineTutorAppApplication.java
@@ -0,0 +1,13 @@
+package com.zen.OnlineTutorApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTutorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTutorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zen/OnlineTutorApp/OnlineTutorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zen.OnlineTutorApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTutorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
